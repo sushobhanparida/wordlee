3df83607742158f6bd978eb68a8903ef0878a428
sushobhanparida <sushobhan96@gmail.com>
2025-08-23 14:55:41 +0530

    feat: Update words.js with 100 diverse 6-letter words

--- server/words.js
+++ server/words.js
@@ -1,5 +1,12 @@
 module.exports = [
   "abacus", "abroad", "absent", "absorb", "abused", "access", "accord", "across", "acting", "action",
   "active", "actual", "adjust", "admire", "adopts", "advice", "advise", "aerial", "affair", "affect",
-  "afford", "afraid", "agency", "agenda", "almost", "always", "amount", "animal", "annual", "answer"
+  "afford", "afraid", "agency", "agenda", "almost", "always", "amount", "animal", "annual", "answer",
+  "anyone", "anyway", "appeal", "appear", "around", "arrive", "artist", "aspect", "assess", "assist",
+  "assume", "assure", "attach", "attack", "attend", "author", "avenue", "backed", "barely", "barrel",
+  "beauty", "became", "become", "before", "behalf", "behind", "belief", "belong", "bellow", "beneath",
+  "beside", "better", "beyond", "border", "borrow", "bottle", "bottom", "bought", "branch", "breath",
+  "bridge", "bright", "broken", "budget", "burden", "bureau", "button", "camera", "campus", "cancel",
+  "cancer", "cannot", "carbon", "career", "carefu", "carpet", "caught", "center", "chance", "change",
+  "charge", "choice", "choose", "chosen", "church", "circle", "client", "closer", "coffee", "column"
 ];
